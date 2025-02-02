3b54c5ee0fb284072bf93efaf0926d887efbc94f
Vikram Sharma <vks8210980102@gmail.com>
2025-02-02 11:13:12 +0530

    Update MyController.java

--- src/main/java/in/vikram/github_learning/controller/MyController.java
+++ src/main/java/in/vikram/github_learning/controller/MyController.java
@@ -16,7 +16,7 @@ public class MyController {
 
     @GetMapping("/m2")
     public String m2(){
-        System.out.println("Hi I am ram ");
+        int x = 34;
         System.out.println("Hi This is vikram Sharma");
         return "Hi I am Ram Sharma";
     }
