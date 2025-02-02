f1e20a59fef8ee88dbdfd481d15ce7bcbe3bf505
Vikram Kumar Sharma <vks8210980102@gmail.com>
2025-02-02 11:01:30 +0530

    added new project

---
+++ src/main/java/in/vikram/github_learning/GithubLearningApplication.java
@@ -0,0 +1,13 @@
+package in.vikram.github_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/vikram/github_learning/GithubLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vikram.github_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
